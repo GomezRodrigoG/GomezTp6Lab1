8acca9340e47b2d9cc75c3aa18a23e222c7609cd
Gomez Rodrigo <gomezrodrigo@gmail.com>
2021-04-26 22:29:47 -0300

    first commit

---
+++ src/gomezpractico6/entities/Directorio.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gomezpractico6.entities;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Directorio {
+//    private HashSet<Cliente> clientes = new HashSet<>();
+    private TreeMap<String,Cliente> clientes = new TreeMap<>();
+    
+    public Cliente agregarCliente(Cliente cliente,String telefono){
+        return clientes.put(telefono, cliente);
+    }
+    
+    public Cliente buscarCliente(String telefono){
+        for(String tel:clientes.keySet()){
+            if(tel.equals(telefono))return clientes.get(tel);
+        }
+        
+        System.out.println("No se encontro un cliente asociado a ese numero telefonico");
+        
+        return null;
+        
+    }
+    
+    // buscarTeléfono() que en base a un apellido nos devuelve una lista con los nros de
+    // teléfono asociados a dicho apellido.
+    public ArrayList<String> buscarTelefono(String apellido){
+        ArrayList<String> toRet = new ArrayList<>();
+        
+        for(String tel:clientes.keySet()){
+            if(clientes.get(tel).getApellido().equals(apellido))toRet.add(tel);
+        }
+        
+        return toRet;
+    }
+    
+    // buscarClientes() que en base a una ciudad nos devuelve una lista con los Clientes
+    // asociados a dicha ciudad
+    public ArrayList<Cliente> buscarClientes(String ciudad){
+        ArrayList<Cliente> toRet = new ArrayList<>();
+        
+        for(String tel:clientes.keySet()){
+            Cliente auxClient = clientes.get(tel);
+            if(auxClient.getCiudad().equals(ciudad))toRet.add(auxClient);
+        }
+        
+        return toRet;
+    }
+    
+    // borrarCliente() que en base a un dni
+    // elimina el cliente del directorio.
+    public boolean borrarCliente(int dni){
+        for(String tel:clientes.keySet()){
+            Cliente auxClient = clientes.get(tel);
+            
+            if(auxClient.getDni() == dni){
+                clientes.remove(tel);
+                return true;
+            };
+        }
+        
+        return false;
+    }
+    
+}
